d805790bb7f98855d6ad1f378355624678603af7
Harin Khakhi <k.harin2306@gmail.com>
2023-12-17 16:15:13 -0800

    v2

--- src/script.py
+++ src/script.py
@@ -1,4 +1,5 @@
-from os import path
+import sys
+import os
 import json
 from tqdm import tqdm
 from dotenv import load_dotenv
@@ -8,6 +9,10 @@ from openai import OpenAI
 import utils as utils
 
 ###################### Configuration ###################### 
+input_file = sys.argv[1]
+output_dir = sys.argv[2]
+output_file = sys.argv[3]
+
 load_dotenv()
 
 openai_client = OpenAI()
@@ -19,20 +24,21 @@ logger = utils.get_logger('script')
 logger.info('script started...')
 logger.info('current configuration: %s', current_config)
 ###########################################################
+if not os.path.isdir(output_dir): os.makedirs(output_dir)
 
-dir = './data/out'
-
-wordlist_file = open(f'./data/wordlist.txt', encoding='utf-8')
+wordlist_file = open(input_file, encoding='utf-8')
 wordlist = wordlist_file.readlines()
 wordlist_file.close()
 
-for line in tqdm(wordlist):
-    word = line.strip()
+new_count = 0
+for line in tqdm(wordlist[:10]):
+    word = line.strip().lower()
 
     # check if already requested
-    if path.isfile(f'{dir}/{word}.json'):
+    if os.path.isfile(f'{output_dir}/{word}.txt'):
         continue
 
+    new_count += 1
     # setting up configuration
     new_config = deepcopy(current_config)
     new_config['messages'].append({
@@ -49,6 +55,24 @@ for line in tqdm(wordlist):
 
     # writing to file
     json_object = response.choices[0].message.content
-    object_file = open(f'{dir}/{word}.txt', 'w')
+    object_file = open(f'{output_dir}/{word}.txt', 'w')
     object_file.write(json_object)
     object_file.close()
+
+print(f'called openai api for {new_count} words')
+
+def get_data(file):
+    with open(file, 'r') as f: 
+        return str(f.read())
+
+
+arr = []
+for filename in os.listdir(output_dir):
+    obj = {
+        'word': filename.split('.')[0],
+        'mnemonic': get_data(os.path.join(output_dir, filename))
+    }
+    arr.append(obj)
+
+with open(output_file, 'w') as output:
+    json.dump(arr, output, indent=4)
